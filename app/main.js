2652ca2d23e1f49135be9bacd84b5e6cbab969bc
renhe <1141198131@qq.com>
2016-07-28 18:58:54 +0800

    sdsdsd

---
+++ app/main.js
@@ -0,0 +1,5 @@
+import React from "react"
+import {render} from "react-dom"
+import A from "./A.jsx"
+
+render(<A />,document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './app/main.js'),
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devServer:{
+        contentBase:'./build'
+    }
+};
